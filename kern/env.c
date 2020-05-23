3a78a60285b4bf85b620a23271ff39ef50aa481f
oferozen <oferozen@gmail.com>
2020-05-23 16:39:25 +0300

    Passed:   fs i/o: OK   check_bc: OK   check_super: OK   check_bitmap: OK

--- kern/env.c
+++ kern/env.c
@@ -428,6 +428,12 @@ env_create(uint8_t *binary, enum EnvType type)
     load_icode(new_env, binary);
     new_env->env_type = type;
 
+    // If this is the file server (type == ENV_TYPE_FS) give it I/O privileges.
+    // LAB 5: Your code here.
+    if (type == ENV_TYPE_FS){
+        new_env->env_tf.tf_eflags |= FL_IOPL_3;
+    }
+
 }
 
 //
